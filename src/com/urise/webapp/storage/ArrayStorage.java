83ccf205c18c6042cb0055ab45ae910b00320664
rockmaester <rockmaester.work@gmail.com>
2023-05-28 12:25:45 +0300

    Lesson #2. Update #2 dy remarks of Igor

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -59,7 +59,7 @@ public class ArrayStorage {
         }
     }
 
-    public int getIndex(String uuid){
+    private int getIndex(String uuid){
         for(int i = 0; i < size; i++){
             if(storage[i].getUuid().equals(uuid)){
                 return i;
